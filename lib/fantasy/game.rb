024c6774baa07820329625a87f59b23d2b4fb841
Jeremy Stephens <viking415@gmail.com>
2008-11-10 17:11:12 -0600

    Oniguruma is freakin' awesome
    - added initial Game parsing

--- lib/fantasy/game.rb
+++ lib/fantasy/game.rb
@@ -1,7 +1,28 @@
 class Fantasy
   class Game
-    @@type_re = %r{<td.+class="ysptblhdr">&nbsp;(?<type>Passing)}
-    @@team_re = %r{<td.+class="yspdetailttl first".+>&nbsp;(?<team>[\w\s]+)</td>}
+    # this regex owns you; it will match type, team, and player
+    @@parsing_re = %r!
+      (?<type>
+        <tr\sclass="yspsctbg">[\s\n]+
+        <td\swidth="\d+%"\sheight="18"\sclass="ysptblhdr">
+          &nbsp; (?<type_name> [\w\s\\]+ )
+        </td>
+      ){0}
+      (?<team>
+        <tr\sclass="ysptblthbody1"\salign="right">[\s\n]+
+          <td\sheight="18"\sclass="yspdetailttl\sfirst"\salign="left">
+            &nbsp; (?<team_name> [\w\s]+ )
+          </td>
+      ){0}
+      (?<player>
+        <tr\sclass="ysprow1"\salign="right">[\s\n]+
+          <td\sclass="first"\salign="left">
+            &nbsp; (?<player_name> [\w\s\.\-']+ )
+          </td>
+      ){0}
+
+      (?:\g<type>|\g<team>|\g<player>)
+    !x
 
     attr_reader :home_team, :away_team
     def initialize(body)
@@ -25,12 +46,23 @@ class Fantasy
     end
 
     def create_players
-      #<table width="100%" border="0" cellspacing="0" cellpadding="0" class="yspwhitebg">
-      # <tr class="yspsctbg">
-      #	  <td width="24%" height="18" class="ysptblhdr">&nbsp;Passing</td>
-      pos, md = @@type_re.matchn(@body, 0)
+      type, team = nil
+      pos, md = @@parsing_re.matchn(@body, 0)
       while pos
-        pos, md = @@type_re.matchn(@body, pos+1)
+        case
+          when md[:type_name]
+            type = md[:type_name]
+          when md[:team_name]
+            team = team ? @home_team : @away_team
+          when md[:player_name]
+            name = md[:player_name]
+            player = team.find_player(name)
+            unless player
+              player = Player.new(name)
+              team.add(player)
+            end
+        end
+        pos, md = @@parsing_re.matchn(@body, pos+1)
       end
     end
   end
